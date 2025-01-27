2a720dee8534c67d6d68ba3338e89e5d72064038
Atluri Nikhil <natluri2001@gmail.com>
2025-01-27 07:47:39 -0500

    Day 2

---
+++ 36 Valid Sudoku.py
@@ -0,0 +1,27 @@
+# Solution 1
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        for i in range(9):
+            row_set = set()
+            col_set = set()
+            for j in range(9):
+                if board[i][j] in row_set:
+                    return False
+                else:
+                    if board[i][j] != ".":
+                        row_set.add(board[i][j])
+                if board[j][i] in col_set:
+                    return False
+                else:
+                    if board[j][i] != ".":
+                        col_set.add(board[j][i])
+                if i%3 == 0 and j%3 == 0:
+                    box_set = set()
+                    for box_i in range(i, i+3):
+                        for box_j in range(j, j+3):
+                            if board[box_i][box_j] in box_set:
+                                return False
+                            else:
+                                if board[box_i][box_j] != ".":
+                                    box_set.add(board[box_i][box_j])
+        return True
