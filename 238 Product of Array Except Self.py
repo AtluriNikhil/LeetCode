ec3b64dda3506f50d2e6da54906d26de8d470107
Atluri Nikhil <natluri2001@gmail.com>
2025-01-27 07:10:53 -0500

    Day 2

--- 238 Product of Array Except Self.py
+++ 238 Product of Array Except Self.py
@@ -5,13 +5,38 @@ class Solution:
         suffix_mult_array = [1]
         product = 1
         for num in nums:
-            prefix_mult_array.append(product*num)
             product = product * num
+            prefix_mult_array.append(product)
+            
         product = 1
         for num in nums[::-1]:
-            suffix_mult_array.insert(0, product*num)
             product = product * num
+            suffix_mult_array.insert(0, product)
+
         result = list()
         for index in range(len(nums)):
             result.append(prefix_mult_array[index] * suffix_mult_array[index+1])
+        return result
+
+# Solution 2
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        result = [1]
+        length = len(nums)
+        product = 1
+        for num in nums:
+            product = product * num
+            result.append(product)
+
+        product = 1
+        for index in range(length-1, -1, -1):
+            product = product*nums[index]
+            nums[index] = product
+        nums.append(1)
+
+        result.pop()
+        nums.pop(0)
+
+        for index in range(length):
+            result[index] = result[index] * nums[index]
         return result
