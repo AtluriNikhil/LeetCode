35e291c6e0d7c571ef8d1a2841d714cc2bb209bc
Atluri Nikhil <natluri2001@gmail.com>
2025-01-28 18:47:36 -0500

    Day 3

--- 128 Longest Consecutive Sequence.py
+++ 128 Longest Consecutive Sequence.py
@@ -23,17 +23,15 @@ class Solution:
         return result
 
 # Solution 2
-# Uses a dictionary where each key represents a number from the input array, and its corresponding value indicates the length of a consecutive sequence with that number as either the upper or lower bound of the sequence.
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         nums = set(nums)
-        table = {}
-        maxval = 0
+        longest = 0
         for num in nums:
-            x = table.get(num - 1, 0)
-            y = table.get(num + 1, 0)
-            val = x + y + 1
-            table[num - x] = val
-            table[num + y] = val
-            maxval = max(maxval, val)
-        return maxval
+            if num - 1 not in nums:
+                length = 1
+                while num + length in nums:
+                    length += 1
+                if length > longest:
+                    longest = length
+        return longest
