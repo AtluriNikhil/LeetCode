88a65253deca1069c32f0daa835fdbe9210dbec3
Atluri Nikhil <natluri2001@gmail.com>
2025-02-04 16:10:12 -0500

    Day 8

---
+++ 20 Valid Parentheses.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {")":"(", "}":"{", "]":"["}
+
+        for char in s:
+            if char not in mapping:
+                stack.append(char)
+            else:
+                if len(stack) != 0 and stack[-1] == mapping[char]:
+                    stack.pop()
+                else:
+                    return False
+        return len(stack) == 0
